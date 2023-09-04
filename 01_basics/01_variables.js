f0570ae7596bce3d5aeb20b0a62e3f2ca41d8f08
alokvns48 <alokvns48@gmail.com>
2023-09-04 22:30:48 +0530

    Day 1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144559
+let accountEmail = "alokvns48@gmail.com"
+var accountPassword = "12345"
+accountCity = "Varanasi"
+
+/*
+ Prefer not to use var due issue with block and functional scope
+ */
+
+// accountId = 2 // /*Not allowed*/
+
+accountEmail = "arjun@gmail.com"
+accountPassword = "54321"
+accountCity = "Ghaziabad"
+
+// console.log(accountEmail)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs ,not browser
+
+/*Data Types*/
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number
+// bigint
+// string => ""
+// boolean => true/FALSE
+// null =>  standalone value
+// undefined
+// symbol => unique
+
+console.log(typeof age)
+
+console.log(typeof undefined)
+
+console.log(typeof null)
